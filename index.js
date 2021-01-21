534e6f2b8e8d5530e6d6fce8a1ccdb0a174c7ea3
Kyle West <kyle@familysearch.org>
2021-01-21 10:41:16 -0700

    [interval] add rotation time

--- index.js
+++ index.js
@@ -3,35 +3,61 @@ const getReviewers = require('./reviewers')
 
 // get the week number (0 - 52) for the week in the year
 // right now this is really naive
-Date.prototype.getWeek = function() {
+Date.prototype.getDayOfYear = function() {
   const firstDayOfYear = new Date(this.getFullYear(), 0, 1);
   const today = new Date(this.getFullYear(), this.getMonth(), this.getDate());
-  const dayOfYear = ((today - firstDayOfYear + 86400000) / 86400000);
-  return Math.ceil(dayOfYear / 7) - 1
+  return Math.round((today - firstDayOfYear + 86400000) / 86400000) - 1;
 };
+
+function parseRotation(rot) {
+  if (!rot) return 7
+
+  const rotStr = String(rot).toLowerCase()
+  const rotNum = parseInt(String(rot).match(/\d+/) || ['1'][0], 10)
+  let rotInterval = 1
+  if (rotStr.includes('day') || rotStr.includes('daily')) {
+    rotInterval = 1
+  }
+  
+  if (rotStr.includes('week')) {
+    rotInterval = 7    
+  }
+
+  if (rotStr.includes('month')) {
+    rotInterval = 30  
+  }
+
+  if (rotStr.includes('year')) {
+    rotInterval = 365
+  }
+
+  console.log({ rotNum, rotStr, rotInterval })
+  return rotNum * rotInterval
+}
 
 async function run() {
   try {
     const reviewerList = core.getInput('reviewers').split(/\s+/);
     const token = core.getInput('token');
     const fetchTeamUsers = core.getInput('fetch-team-users') || false;
+    const rotationDays = parseRotation(core.getInput('rotation'));
 
-    const selectThisWeeksReviewer = async (...dateArgs) => {
+    const selectTodaysReviewer = async (...dateArgs) => {
       let reviewers = reviewerList.map(x => x.replace(/^@/, '')), getTeam = () => {};
-      
+
       if (fetchTeamUsers) {
         let data = await getReviewers({ token, reviewers: reviewerList })
         reviewers = data.reviewers
         getTeam = data.getTeam()
       }
       
-      const weekNum = new Date(...dateArgs).getWeek()
-      const reviewer = reviewers[weekNum % reviewers.length]
+      const dayOfYear = new Date(...dateArgs).getDayOfYear()
+      const reviewer = reviewers[Math.floor(dayOfYear / rotationDays) % reviewers.length]
 
-      return { weekNum, idx: weekNum % reviewers.length, reviewer, team: getTeam(reviewer) }
+      return { reviewer, team: getTeam(reviewer) }
     } 
     
-    const { reviewer, team } = await selectThisWeeksReviewer()
+    const { reviewer, team } = await selectTodaysReviewer()
 
     core.setOutput('reviewer', reviewer);
     core.setOutput('team', team || reviewer); // default to just the reviewer if no team given
